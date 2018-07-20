874ad37593f64b11d45ebf07751c9e9743a049aa
Rhossan <rhossan93@gmail.com>
2018-07-19 20:13:26 -0400

    Able to retrieve relevant kitty info based on ID, moving onto UI

--- src/components/Browser.js
+++ src/components/Browser.js
@@ -5,7 +5,10 @@ import KittyCoreABI from '../contracts/KittyCoreABI.json';
 import { CONTRACT_NAME, CONTRACT_ADDRESS } from '../config';
 
 class Browser extends Component {
-
+  constructor() {
+    super();
+    this.state = {count: 0}
+  }
   componentDidMount() {
      const web3 = new Web3(window.web3.currentProvider);
 
@@ -22,17 +25,48 @@ class Browser extends Component {
        contractName: CONTRACT_NAME,
        web3Contract: kittyContract
      });
+     debugger
+     console.log(this.context.drizzle);
+
   }
 
+  click(event){
+    event.preventDefault();
+    this.setState({count: this.state.count+1 });
+    var state = this.context.drizzle.store.getState();
+     if (state.drizzleStatus.initialized) {
+
+
+       const dataKey = this.context.drizzle.contractList[0].methods.getKitty(123123).call();
+       var result = 0;
+       dataKey.then(function(result) {
+         console.log(result) //will log results.
+       })
+
+       // birthTime - convert unix epoch time
+       // generation
+       // genes
+       // for image - is stored on the web server -
+       // storage.googleapis.com/ck-kitty-image/0x06012c8cf97bead5deae237070f9587f8e7a266d/123123.svg
+       // where 0x06012c8cf97bead5deae237070f9587f8e7a266d is ethereum address for CryptoKittiesCore
+       // and 123123 is kittyId
+
+
+       var account = result;
+
+     }
+  }
+
+
   render() {
-    // const dataKey = this.context.drizzle.contracts.CryptoKitties.getKitty.cachecall(123123);
-    // <h1>{dataKey}</h1>
+
 
     return (
       <div className="browser">
         <h1>
           Kitty Browser
         </h1>
+        <button onClick={this.click.bind(this)}>click me</button>
         {/* Input to type in the kitty ID here */}
 
         {/* Display Kitty info here */}
